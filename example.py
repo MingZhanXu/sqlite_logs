667a2d1c4d1094cc4564142c7af3dc3ef2a008c6
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2025-02-07 16:29:25 +0800

    增加了主機資訊，豐富了log內容，修改了SQL建立表格的方式

---
+++ example.py
@@ -0,0 +1,54 @@
+import os
+from sqlite_log import SQLiteLog, ReadLog
+import time
+
+if __name__ == "__main__":
+    from tqdm import tqdm
+
+    db_folder = os.path.join("logs", "test")
+    logger = SQLiteLog(db_folder=db_folder)
+
+    # 範例：如何傳入 except 執行函數，也可傳入參數
+    def error():
+        return "error"
+
+    @logger.try_except(error_return=error())
+    def delay():
+        time.sleep(1)
+        raise
+
+    print(delay())
+
+    # 範例：如何使用裝飾器
+    @logger.try_except
+    def division(a, b, c=3):
+        return (a / b) + c
+
+    division(1, 2, c=4)
+    # 執行多次 division 函數，並捕獲異常
+    for _ in tqdm(range(50)):
+        division(1, 0)
+        division(1, 2)
+
+    # 範例：如何讀取數據庫中的 log 及 error 訊息 (格式化版本)
+    read_log = ReadLog(db_folder=db_folder)
+    data = read_log.get_logs()
+    error_data = read_log.get_error()
+    info = read_log.get_info()
+    info = [name[1] for name in info]
+    data = [{item: d[i] for i, item in enumerate(info)} for d in data]
+    error_data = [{item: d[i] for i, item in enumerate(info)} for d in error_data]
+    # for d in data:
+    #     print(d)
+    #
+    # for d in error_data:
+    #     print(d)
+
+    print()
+    for key, value in error_data[0].items():
+        if key == "traceback":
+            value = value.replace("\\\\", "\\")
+        print(f"{key}: {value}")
+    print()
+    for key, value in data[0].items():
+        print(f"{key}: {value}")
--- sqlite_log.py
+++ sqlite_log.py
@@ -5,9 +5,111 @@ import sqlite3
 import os
 import atexit
 import time
+import threading
+import platform
+import getpass
+import socket
+import subprocess
+
+from typing import Type
+from dataclasses import dataclass
 
 # 設定單個資料庫大小上限為 100 MB
 MAX_DB_SIZE = 100 * 1024 * 1024
+
+
+def get_computer_name():
+    system_name = platform.system()
+
+    # Windows
+    if system_name == "Windows":
+        return os.environ.get("COMPUTERNAME", socket.gethostname())
+
+    # macOS
+    elif system_name == "Darwin":
+        try:
+            return (
+                subprocess.check_output(["scutil", "--get", "ComputerName"])
+                .decode("utf-8")
+                .strip()
+            )
+        except subprocess.CalledProcessError:
+            return socket.gethostname()
+
+    # Linux
+    elif system_name == "Linux":
+        try:
+            return (
+                subprocess.check_output(["hostnamectl", "--static"])
+                .decode("utf-8")
+                .strip()
+            )
+        except subprocess.CalledProcessError:
+            return socket.gethostname()
+
+    # 其他系統（預設使用 socket.gethostname()）
+    return socket.gethostname()
+
+
+@dataclass
+class SQLType:
+    """SQLite 與 Python 資料型態對應表"""
+
+    sql_type: str
+    py_type: Type
+
+
+@dataclass
+class LoggerType:
+    """Logger 資料庫表格欄位名稱與資料型態對應表"""
+
+    field_name: str
+    sql_type: SQLType
+
+
+# log 資料庫表格欄位名稱與資料型態
+LOGER_TABE_INFO = [
+    LoggerType("host_inof", SQLType("TEXT", str)),
+    LoggerType("timestamp", SQLType("TEXT", str)),
+    LoggerType("type", SQLType("TEXT", str)),
+    LoggerType("function_file_name", SQLType("TEXT", str)),
+    LoggerType("function_line_number", SQLType("INTEGER", int)),
+    LoggerType("function_name", SQLType("TEXT", str)),
+    LoggerType("args", SQLType("TEXT", str)),
+    LoggerType("kwargs", SQLType("TEXT", str)),
+    LoggerType("thread_name", SQLType("TEXT", str)),
+    LoggerType("thread_id", SQLType("INTEGER", int)),
+    LoggerType("pid", SQLType("INTEGER", int)),
+    LoggerType("message", SQLType("TEXT", str)),
+    LoggerType("extra_info", SQLType("TEXT", str)),
+    LoggerType("function_time", SQLType("REAL", float)),
+    LoggerType("traceback", SQLType("TEXT", str)),
+]
+
+
+class Logger:
+    def __init__(
+        self,
+        log_type,
+        function_file_name,
+        function_line_number,
+        function_name,
+        args,
+        kwargs,
+        message,
+        function_time,
+        traceback,
+    ):
+        self.timestamp = datetime.datetime.now().isoformat()
+        self.log_type = log_type
+        self.function_file_name = function_file_name
+        self.function_line_number = function_line_number
+        self.function_name = function_name
+        self.args = args
+        self.kwargs = kwargs
+        self.message = message
+        self.function_time = function_time
+        self.traceback = traceback
 
 
 class SQLiteLog:
@@ -17,6 +119,7 @@ class SQLiteLog:
         wal=True,
         max_db_size=MAX_DB_SIZE,
     ):
+        self.computer_name = get_computer_name()
         if not os.path.exists(db_folder):
             os.makedirs(db_folder)
         self.db_size = 0
@@ -54,19 +157,13 @@ class SQLiteLog:
                 self.create_table()
 
     def create_table(self):
+        self.field_name = [field.field_name for field in LOGER_TABE_INFO]
+        self.field_type = [field.sql_type.sql_type for field in LOGER_TABE_INFO]
         self.cursor.execute(
-            """
+            f"""
             CREATE TABLE IF NOT EXISTS logs (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                timestamp TEXT,
-                type TEXT,
-                line_number INTEGER,
-                function_name TEXT,
-                args TEXT,
-                kwargs TEXT,
-                message TEXT,
-                function_time REAL,
-                traceback TEXT
+                {", ".join([f"{field} {field_type}" for field, field_type in zip(self.field_name, self.field_type)])}
             )
             """
         )
@@ -76,39 +173,24 @@ class SQLiteLog:
         self.conn.commit()
         self.check_and_switch_db()
 
-    def log_to_db(self, log_type, messages):
+    def log_to_db(self, messages):
         self.check_and_switch_db()
-        timestamp = datetime.datetime.now().isoformat()
-        (
-            line_number,
-            function_name,
-            args,
-            kwargs,
-            message,
-            function_time,
-            traceback_str,
-        ) = messages
+        field_str = f"{', '.join(self.field_name)}"
+        input_datas = f"{', '.join(['?' for _ in range(len(self.field_name))])}"
+        print(f"INSERT INTO logs ({field_str}) VALUES ({input_datas})")
+        print(len(messages))
         self.cursor.execute(
-            """
-            INSERT INTO logs (timestamp, type, line_number, function_name, args, kwargs, message, function_time, traceback)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            f"""
+            INSERT INTO logs ({field_str}) VALUES ({input_datas})
             """,
-            (
-                timestamp,
-                log_type,
-                line_number,
-                function_name,
-                args,
-                kwargs,
-                message,
-                function_time,
-                traceback_str,
-            ),
+            messages,
         )
         self.conn.commit()
-        self.db_size += len(log_type) + len(str(messages)) + 100
+        self.db_size += len(str(messages)) + 100
 
-    def try_except(self, func=None, *, error_return=None):
+    def try_except(
+        self, func=None, *, success_type="LOG", extra_info="", error_return=None
+    ):
         """
         tre_except 裝飾器，用於捕獲函數執行過程中的異常，並將異常信息記錄到數據庫中
         error_return: 當函數執行出現異常時，返回的默認值，可使用此參數來自定義異常時的返回值
@@ -118,38 +200,66 @@ class SQLiteLog:
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            start_time = time.time()
+            start_time = datetime.datetime.now()
+            start_time_timestamp = start_time.timestamp()
+            start_time = start_time.isoformat()
+
+            host_info = platform.uname()
+            host_info = (
+                f"Computer  : {self.computer_name}\n"
+                f"User      : {getpass.getuser()}\n"
+                f"System    : {host_info.system}\n"
+                f"Node      : {host_info.node}\n"
+                f"Release   : {host_info.release}\n"
+                f"Version   : {host_info.version}\n"
+                f"Machine   : {host_info.machine}"
+            )
+
             stack = traceback.extract_stack()[-2]
             line_number = stack.lineno
+            file_name = stack.filename
+            name = func.__name__
+            args_str = str(args)
+            kwargs_str = str(kwargs)
+
+            current_thread = threading.current_thread()
+            thread_name = current_thread.name
+            thread_id = current_thread.ident
+            pid = os.getpid()
+            messages = [
+                file_name,
+                line_number,
+                name,
+                args_str,
+                kwargs_str,
+                thread_name,
+                thread_id,
+                pid,
+            ]
             try:
                 result = func(*args, **kwargs)
-                end_time = time.time()
-                function_time = end_time - start_time
+                log_type = success_type
+                end_time_timestamp = datetime.datetime.now().timestamp()
+                function_time = end_time_timestamp - start_time_timestamp
                 message = f"result: {repr(result)}"
                 messages = (
-                    line_number,
-                    func.__name__,
-                    str(args),
-                    str(kwargs),
-                    message,
-                    function_time,
-                    None,
+                    [host_info, start_time, log_type]
+                    + messages
+                    + [message, extra_info, function_time, None]
                 )
-                self.log_to_db("LOG", messages)
+                self.log_to_db(messages)
                 return result
             except Exception as e:
-                end_time = time.time()
-                function_time = end_time - start_time
+                log_type = "ERROR"
+                end_time_timestamp = datetime.datetime.now().timestamp()
+                function_time = end_time_timestamp - start_time_timestamp
+                error_msg = f"{e.__class__.__name__}: {str(e)}"
                 messages = (
-                    line_number,
-                    func.__name__,
-                    str(args),
-                    str(kwargs),
-                    str(e),
-                    function_time,
-                    traceback.format_exc(),
+                    [host_info, start_time, log_type]
+                    + messages
+                    + [error_msg, extra_info, function_time, traceback.format_exc()]
                 )
-                self.log_to_db("ERROR", messages)
+                self.log_to_db(messages)
                 return error_return
 
         return wrapper
@@ -172,6 +282,21 @@ class ReadLog:
             atexit.register(self.close)
         except sqlite3.OperationalError as e:
             print(f"Connect to {self.db_name}_{db_index}.db failed")
+            print(f"Error: {e}")
+            exit(-1)
+
+    def get_data(self, type):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(
+                """
+                SELECT * FROM logs where type = ?
+                """,
+                (type,),
+            )
+            return cursor.fetchall()
+        except sqlite3.OperationalError as e:
+            print(f"get_data from {self.db_name}_{self.db_index}.db failed")
             print(f"Error: {e}")
             exit(-1)
 
@@ -204,6 +329,9 @@ class ReadLog:
             exit(-1)
 
     def get_info(self):
+        """
+        獲取 logs 表格的資訊
+        """
         try:
             cursor = self.conn.cursor()
             cursor.execute(
@@ -218,6 +346,9 @@ class ReadLog:
             exit(-1)
 
     def next_db(self):
+        """
+        獲取下一個 log 檔案
+        """
         try:
             self.conn.close()
             self.conn = sqlite3.connect(f"{self.db_name}_{self.db_index}.db")
@@ -231,51 +362,3 @@ class ReadLog:
             self.conn.close()
         except sqlite3.OperationalError as e:
             print(f"Close {self.db_name}_{self.db_index}.db failed")
-
-
-if __name__ == "__main__":
-    from tqdm import tqdm
-
-    db_folder = os.path.join("logs", "test")
-    logger = SQLiteLog(db_folder=db_folder)
-
-    # 範例：如何傳入 except 執行函數，也可傳入參數
-    def error():
-        return "error"
-
-    @logger.try_except(error_return=error())
-    def delay():
-        time.sleep(1)
-        raise
-
-    print(delay())
-
-    # 範例：如何使用裝飾器
-    @logger.try_except
-    def division(a, b):
-        return a / b
-
-    # 執行多次 division 函數，並捕獲異常
-    for _ in tqdm(range(50)):
-        division(1, 0)
-        division(1, 2)
-
-    # 範例：如何讀取數據庫中的 log 及 error 訊息 (格式化版本)
-    read_log = ReadLog(db_folder=db_folder)
-    data = read_log.get_logs()
-    error_data = read_log.get_error()
-    info = read_log.get_info()
-    info = [name[1] for name in info]
-    data = [{item: d[i] for i, item in enumerate(info)} for d in data]
-    error_data = [{item: d[i] for i, item in enumerate(info)} for d in error_data]
-    for d in data:
-        print(d)
-
-    for d in error_data:
-        print(d)
-
-    print()
-    for key, value in error_data[0].items():
-        if key == "traceback":
-            value = value.replace("\\\\", "\\")
-        print(f"{key}: {value}")
