7d6df7bf930ebcbb6279ba1f2bb74899e5fbfbe1
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2025-02-19 16:13:31 +0800

    修改is_record的判斷方式，由str改為bool，並使用copy()來設定參數。

--- src/sqlite_log/logger.py
+++ src/sqlite_log/logger.py
@@ -3,12 +3,10 @@ import datetime
 import traceback
 import os
 import threading
-import getpass
-import json
 import re
 
 from typing import List, Optional, Callable, Any
-from .logger_type import LoggerInfo, LoggerOutput, SQLiteLog, Union
+from .logger_type import LoggerInfo, LoggerOutput, SQLiteLog, Union, LoggerMark
 from .get_system_info import SystemInfo
 
 # 正則表達式"#*:* "
@@ -24,7 +22,7 @@ class Logger:
         logger_info: LoggerInfo = LoggerInfo(),
         logger_output: Optional[LoggerOutput] = None,
     ):
-        self.__logger_info = logger_info
+        self.__logger_info = logger_info.copy()
         if logger_output is None:
             logger_output = LoggerOutput()
         self.__logger_output = logger_output
@@ -36,31 +34,34 @@ class Logger:
         func_tag = self.__logger_info.get_field_value()
         if func_doc:
             func_re = re.findall(TAG_REGULAR, func_doc)
-            func_re_tag = {
-                k.lstrip("#"): v
-                for k, v in (item.split(":") for item in func_re)
-                if k.lstrip("#") in func_tag.keys()
-            }
+            func_re_tag = dict()
+            for item in func_re:
+                key, value = item.lstrip("#").split(":")
+                if key in func_tag.keys():
+                    if key in LoggerMark.__args__:
+                        func_re_tag[key] = value
+                    else:
+                        func_re_tag[key] = False if value.lower() == "false" else True
             self.__logger_info.update_record(func_re_tag)
 
         return func_tag
 
     def __set_system_logger(self, logger_info: LoggerInfo) -> None:
         is_record = logger_info.get_is_record()
-        if is_record["computer"] == "true":
+        if is_record["computer"] == True:
             logger_info.set_field_value("computer", self.__system_info.get_host_info())
-        if is_record["cpu"] == "true":
+        if is_record["cpu"] == True:
             logger_info.set_field_value("cpu", self.__system_info.get_cpu_info())
-        if is_record["memory"] == "true":
+        if is_record["memory"] == True:
             logger_info.set_field_value("memory", self.__system_info.get_memory_info())
-        if is_record["gpu"] == "true":
+        if is_record["gpu"] == True:
             logger_info.set_field_value("gpu", self.__system_info.get_gpu_info())
-        if is_record["host"] == "true":
+        if is_record["host"] == True:
             logger_info.set_field_value("host", self.__system_info.get_host_info())
 
     def __set_thread_logger(self, logger_info: LoggerInfo) -> None:
         is_record = logger_info.get_is_record()
-        if is_record["thread"] == "true":
+        if is_record["thread"] == True:
             logger_info.set_field_value("thread_name", threading.current_thread().name)
             logger_info.set_field_value("thread_id", threading.current_thread().ident)
             logger_info.set_field_value("process_id", os.getpid())
@@ -104,11 +105,12 @@ class Logger:
                 error_return=error_return,
             )
         self.__parse_func_doc(func)
-        logger_info = self.__logger_info
-        is_record = logger_info.get_is_record()
+        __logger_info = self.__logger_info
+        is_record = __logger_info.get_is_record()
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            logger_info = __logger_info.copy()
             start_time = datetime.datetime.now()
             start_time_timestamp = start_time.timestamp()
             start_time = start_time.isoformat()
@@ -116,7 +118,7 @@ class Logger:
             self.__set_system_logger(logger_info)
             self.__set_thread_logger(logger_info)
 
-            if is_record["function"] == "true":
+            if is_record["function"] == True:
                 logger_info.set_field_value("function_name", func.__name__)
                 logger_info.set_field_value("args", str(args))
                 logger_info.set_field_value("kwargs", str(kwargs))
@@ -125,13 +127,13 @@ class Logger:
             try:
                 return_value = func(*args, **kwargs)
             except Exception as e:
-                if is_record["function"] == "true":
+                if is_record["function"] == True:
                     logger_info.set_field_value("traceback", traceback.format_exc())
                     error_type = f"Function error: {type(e).__name__}"
                     logger_info.set_field_value("error_type", error_type)
                 return_value = self.__run_error_return(error_return)
             finally:
-                if is_record["function"] == "true":
+                if is_record["function"] == True:
                     end_time_timestamp = datetime.datetime.now().timestamp()
                     function_time = end_time_timestamp - start_time_timestamp
                     return_value_str = str(return_value)
