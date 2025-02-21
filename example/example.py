5db5be2a5fefc2ba9580cb7fc89241b5df994274
MingZhanXu <MingZhan.Xu.vm3@gmail.com>
2025-02-21 15:50:57 +0800

    增加config形態限定

--- example/example.py
+++ example/example.py
@@ -1,8 +1,8 @@
-from src.sqlite_log import Logger
-from src.sqlite_log import SQLiteLog, LoggerInfo, LoggerTagValue
+from src.logger import Logger
+from src.logger import SQLiteLog, LoggerInfo, LoggerTagValue
 
 if __name__ == "__main__":
-    config = {"cpu": False, "level": "DEBUG"}
+    config: LoggerTagValue = {"cpu": False, "level": "DEBUG"}
     logger_info = LoggerInfo(config)
     sqlite_log = SQLiteLog
     logger = Logger(logger_info=logger_info, logger_output=sqlite_log)
